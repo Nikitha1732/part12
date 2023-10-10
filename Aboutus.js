ba2323341df4131b415a174f11ed5475015ef890
Nikitha1732 <116977240+Nikitha1732@users.noreply.github.com>
2023-10-11 00:13:43 +0530

    Add files via upload

---
+++ Aboutus.js
@@ -0,0 +1,11 @@
+import React from 'react';
+const Aboutus=()=>{
+    return(
+        <div>
+            <center>
+                <h4>AboutUs Page</h4>
+            </center>
+        </div>
+    )
+}
+export default Aboutus;
